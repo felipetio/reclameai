b74455f66c1d45a802c16a05ebc2566d0ca09890
Felipe Vieira <felipe.tio@gmail.com>
2019-08-21 10:19:11 -0300

    Automatically geocode orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,13 @@
 class Order < ApplicationRecord
+  COUNTRY = "Brazil"
   belongs_to :user
+
+  geocoded_by :full_address, latitude: :lat, longitude: :lng
+  #after_validation :geocode, if: ->(obj){ obj.address_changed? or obj.city_changed? or obj.state_changed? }
+  after_validation :geocode
+
+
+  def full_address
+    [address, number, city, state, COUNTRY].join(', ')
+  end
 end
